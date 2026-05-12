e758fa1c2d6e29cf0118824ea7273218e28db6fc
Dayane <dayanebmartins@gmail.com>
2026-05-12 15:04:00 -0300

    refactor: refatora a manipulação das respostas da api em formato json, acessando por chaves

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, request
-# import requests
-import os
 from services.langflow_services import call_langflow
 from services.product_services import call_api_products
 from database.customers import user_database
 
-import json
-import re
+
 
 
 app = Flask(__name__)
@@ -18,31 +15,30 @@ def home(id_client):
 
     # Dados iniciais
     answer = "Olá, Dayane! Como posso ajudar você hoje?"
-    produtos_recomendados = []
+    recommended_products = []
     products = call_api_products()
 
     try:
         # Fluxo do usuário envia pergunta sobre faq ou pedidos:
-        if request.method == 'POST':
-            question = request.form.get('question')
-            
+        if request.method == "POST":
+            question = request.form.get("question")
+
             if question:
                 response = call_langflow(question, user, products)
-                print(response)
+                answer = response["chat_answer"]
+                recommended_products = response["recommendations"]
 
         # Fluxo que apenas exibe recomendações:
-        else:
-            response = call_langflow('', user, products)
-            print(response)
-
-    #     ai_text = raw_data['outputs'][0]['outputs'][0]['results']['message']['text']
+        # else:
+        #     response = call_langflow('', user, products)
+        #     print(response)
 
     except Exception as e:
         print(f"Erro: {e}")
         answer = "Olá! Como posso ajudar?"
-        produtos_recomendados = products[:3]
+        #produtos_recomendados = products[:3]
 
-    return render_template('index.html', answer=answer, user=user, produtos=produtos_recomendados)
+    return render_template('index.html', answer=answer, user=user, products_list=recommended_products)
 
 
 app.run(debug=True)
