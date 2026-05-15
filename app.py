795abd924e380f9ea3877cfb282c10f33878921b
Dayane <dayanebmartins@gmail.com>
2026-05-15 13:19:33 -0300

    refactor: separação da  lógica de processamento das entradas do usuário  da rota principal em controller

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
-from services.langflow_services import call_langflow
-from services.product_services import get_products_for_ai
 from database.customers import user_database
-from services.email_services import send_email
+from controllers.chat_controller import process_user_message
 
 
 app = Flask(__name__)
@@ -15,28 +13,14 @@ def home(id_client):
     # Dados iniciais
     answer = f'Olá, {user["nome"]} Como posso ajudar você hoje?'
     recommended_products = []
-    products = get_products_for_ai()
+  
+    # Fluxo do usuário envia pergunta sobre faq,recomendação ou pedidos:
+    if request.method == "POST":
+        question = request.form.get("question")
+        
+        if question:
+            answer, recommended_products = process_user_message(question, user)
 
-    try:
-        # Fluxo do usuário envia pergunta sobre faq,recomendação ou pedidos:
-        if request.method == "POST":
-            question = request.form.get("question")
-
-            if question:
-                response = call_langflow(question, user, products)
-                answer = response["chat_answer"]
-                recommended_products = response["recommendations"]
-
-                if response['action_required'] == "send_email":
-                    send_email(
-                        to_email=user["email"],
-                        subject="Informações sobre pedido XXX",
-                        body=response['email_body']
-                    )
-
-    except Exception as e:
-        print(f"Erro: {e}")
-        answer = "Desculpe, ocorreu um erro ao processar sua solicitação. Tente novamente em instantes."
 
     return render_template('index.html', answer=answer, user=user, products_list=recommended_products)
 
---
+++ controllers/chat_controller.py
@@ -0,0 +1,28 @@
+from services.langflow_services import call_langflow
+from services.product_services import get_products_for_ai
+from services.email_services import send_email
+
+
+def process_user_message(question, user):
+
+    products = get_products_for_ai()
+    recommended_products = []
+
+    try:
+        products = get_products_for_ai()
+        response = call_langflow(question, user, products)
+        answer = response["chat_answer"]
+        recommended_products = response["recommendations"]
+
+        if response['action_required'] == "send_email":
+            send_email(
+                to_email=user["email"],
+                subject="Informações sobre pedido XXX",
+                body=response['email_body']
+            )
+
+    except Exception as e:
+        print(f"Erro: {e}")
+        answer = "Desculpe, ocorreu um erro ao processar sua solicitação. Tente novamente em instantes."
+
+    return answer, recommended_products
